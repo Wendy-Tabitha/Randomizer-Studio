7c6716d358f8839db3d3161a9a4e1f3cac162893
Wendy-Tabitha <wendytbt4@gmail.com>
2025-06-20 21:39:17 +0300

    Add WritingPromptsPage component for generating writing prompts

---
+++ src/app/(tools)/writing-prompts/page.tsx
@@ -0,0 +1,9 @@
+import { WritingPromptGenerator } from "@/components/writing-prompt-generator";
+
+export default function WritingPromptsPage() {
+  return (
+    <div className="flex flex-col items-center justify-start py-8 px-4">
+      <WritingPromptGenerator />
+    </div>
+  );
+}
