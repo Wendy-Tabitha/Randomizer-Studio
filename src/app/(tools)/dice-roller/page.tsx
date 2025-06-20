c17e0ace7711d86b6ffbc72e94bd2e76e84233ba
Wendy-Tabitha <wendytbt4@gmail.com>
2025-06-20 21:38:20 +0300

    Add DiceRollerPage component for dice roller functionality

---
+++ src/app/(tools)/dice-roller/page.tsx
@@ -0,0 +1,9 @@
+import { DiceRollerForm } from "@/components/dice-roller-form";
+
+export default function DiceRollerPage() {
+  return (
+    <div className="flex flex-col items-center justify-start py-8 px-4">
+      <DiceRollerForm />
+    </div>
+  );
+}
