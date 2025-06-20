848bda8e56cc32065276fde025ac9db801edae6f
Wendy-Tabitha <wendytbt4@gmail.com>
2025-06-20 21:39:06 +0300

    Add PasswordGeneratorPage component for password generation functionality

---
+++ src/app/(tools)/password-generator/page.tsx
@@ -0,0 +1,9 @@
+import { PasswordGeneratorForm } from "@/components/password-generator-form";
+
+export default function PasswordGeneratorPage() {
+  return (
+    <div className="flex flex-col items-center justify-start py-8 px-4">
+      <PasswordGeneratorForm />
+    </div>
+  );
+}
